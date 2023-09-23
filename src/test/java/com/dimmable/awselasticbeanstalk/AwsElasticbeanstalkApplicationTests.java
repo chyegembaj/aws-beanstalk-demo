51ebff9b3e4843e0b7c22c53b5ec405567ff6b72
Chidimma <chyegembaj@yahoo.com>
2023-09-23 17:43:34 -0600

    First commit, CICD deployment with Springboot app with Elastic Beanstalk

---
+++ src/test/java/com/dimmable/awselasticbeanstalk/AwsElasticbeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimmable.awselasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticbeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
